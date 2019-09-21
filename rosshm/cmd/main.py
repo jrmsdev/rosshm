64bff2bf81d9b9d64197c1697562ebe43c563259
Jeremías <jrmsdev@gmail.com>
2019-09-21 15:44:32 -0300

    set runtime home dir

--- rosshm/cmd/main.py
+++ rosshm/cmd/main.py
@@ -3,17 +3,26 @@
 
 import sys
 import subprocess as proc
-from os import path
+from os import path, getenv
 
 from rosshm import log
 from rosshm.cmd import flags
 
+def _gethome():
+	h = getenv('ROSSHM_HOME', None)
+	if h is None:
+		h = sys.exec_prefix
+	return path.abspath(h)
+
 def main():
 	args = flags.parse()
+	cfgfn = path.abspath(path.expanduser(args.config))
 	cmd = (
 	'uwsgi',
 		'--need-plugin', 'python3',
-		'--ini', path.expanduser(args.config),
+		'--set-ph', f"rosshm-home={_gethome()}",
+		'--touch-reload', cfgfn,
+		'--ini', cfgfn,
 	)
 	try:
 		log.debug(f"run {cmd}")
---
+++ rosshm/wsgi.py
@@ -0,0 +1,8 @@
+# Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+# See LICENSE file.
+
+from bottle import Bottle
+
+__all__ = ['application']
+
+application = Bottle()
