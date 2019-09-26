fd952109a9dc5cdbe2acb97b60812b9e96ee323f
Jeremías <jrmsdev@gmail.com>
2019-09-26 15:19:34 -0300

    response header Content-Security-Policy

--- rosshm/wapp/plugin/response.py
+++ rosshm/wapp/plugin/response.py
@@ -16,5 +16,9 @@ class Plugin(object):
 		def wrapper(*args, **kwargs):
 			log.debug(f"apply wrapper {self.name}")
 			bottle.response['Server'] = 'rosshm'
+			csp = "object-src 'self';"
+			csp += " default-src 'self';"
+			csp += " script-src 'self';"
+			bottle.response['Content-Security-Policy'] = csp
 			return callback(*args, **kwargs)
 		return wrapper
