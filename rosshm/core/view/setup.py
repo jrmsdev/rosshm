46e1675af8881e8bfec09a88f0e00bfb6b468127
Jeremías <jrmsdev@gmail.com>
2019-09-27 22:13:49 -0300

    link to create database if no status info

--- rosshm/core/view/setup.py
+++ rosshm/core/view/setup.py
@@ -2,8 +2,9 @@
 # See LICENSE file.
 
 import bottle
+from os import path
 
-from rosshm import log
+from rosshm import log, config
 from rosshm.db import db
 
 def redirect(rpath = ''):
@@ -13,11 +14,15 @@ def redirect(rpath = ''):
 @bottle.view('core/setup/index.html')
 def view():
 	log.debug('view')
+	dbfn = path.abspath(path.join(config.get('datadir'), 'rosshm.db'))
+	log.debug(f"dbfn {dbfn}")
+	status = {}
+	error = None
 	try:
 		conn = db.connect(dbfn)
 		status = db.status(conn)
 		conn.close()
 	except db.Error as err:
 		log.error(f"check database: {err}")
-		status = {'error': str(err)}
-	return {'status': status}
+		error = str(err)
+	return {'error': error, 'status': status}
