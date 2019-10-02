c0503384a65ada186ed5bee645d9e22a4d2153b5
Jeremías <jrmsdev@gmail.com>
2019-10-02 18:26:01 -0300

    test default database config

--- t/config_test.py
+++ t/config_test.py
@@ -1,6 +1,15 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from rosshm import config
+
 def test_config(testing_config):
 	with testing_config():
-		pass
+		assert config.filename().endswith('rosshm.ini')
+
+def test_database(testing_config):
+	with testing_config():
+		db = config.database()
+		assert db['driver'] == 'sqlite'
+		assert db['name'].endswith('rosshmdb.sqlite')
+		assert db['config'] == ''
