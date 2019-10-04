0ada6ace002501af4633e8c5cf48e8211ab494bc
Jeremías <jrmsdev@gmail.com>
2019-10-04 03:20:51 -0300

    install devel venv requirements

--- devel/venv.sh
+++ devel/venv.sh
@@ -4,4 +4,7 @@ PIP=${ENVDIR}/bin/pip
 python3 -m venv --system-site-packages --symlinks --upgrade \
 	--prompt rosshmdev ${ENVDIR}
 ${PIP} install -U pip
+${PIP} install -U -r devel/requirements.txt
+${PIP} install -U -r requirements.txt
+${PIP} uninstall -y rosshm
 ${PIP} install -e .
