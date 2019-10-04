1eb7b11ee198f573f77991e371e159aa88e9acc4
Jeremías <jrmsdev@gmail.com>
2019-10-03 23:12:14 -0300

    test checkdb makedirs and no status

--- t/db_test.py
+++ t/db_test.py
@@ -1,11 +1,12 @@
 # Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
 # See LICENSE file.
 
+from os import path
 from pytest import raises
 from unittest.mock import Mock
 
 from rosshm import config
-from rosshm.db import db
+from rosshm.db import db, check
 from rosshm.db.check import checkdb
 from rosshm.db.reg import register
 
@@ -34,3 +35,32 @@ def test_checkdb(testing_db):
 		assert not checkdb(config)
 	with testing_db() as conn:
 		assert checkdb(config, conn = conn)
+
+def test_checkdb_makedirs(testing_db):
+	with testing_db() as conn:
+		makedirs = check.makedirs
+		isfile = check.path.isfile
+		try:
+			datadir = path.join(path.sep, 'testing')
+			check.makedirs = Mock()
+			check.path.isfile = Mock(return_value = False)
+			config._cfg.set('rosshm', 'datadir', datadir)
+			config._cfg.set('rosshm', 'db.name', 'testing.db')
+			checkdb(config, conn = conn)
+			check.path.isfile.assert_called_with(path.join(datadir, 'testing.db.sqlite'))
+			check.makedirs.assert_called_with(datadir, exist_ok = True, mode = 0o750)
+		finally:
+			del check.makedirs
+			check.makedirs = makedirs
+			del check.path.isfile
+			check.path.isfile = isfile
+
+def test_checkdb_no_status(testing_db):
+	with testing_db() as conn:
+		status = check.db.status
+		try:
+			check.db.status = Mock(return_value = None)
+			assert not checkdb(config, conn = conn)
+		finally:
+			del check.db.status
+			check.db.status = status
