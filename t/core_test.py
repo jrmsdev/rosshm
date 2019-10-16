e31aae7c579e48446624beabb1b70c0092149862
Jeremías <jrmsdev@gmail.com>
2019-10-16 14:17:08 -0300

    test core wapp init

---
+++ t/core_test.py
@@ -0,0 +1,15 @@
+# Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+# See LICENSE file.
+
+from unittest.mock import call
+
+from rosshm.core.view import setup, status
+
+def test_init_setup(testing_wapp):
+	with testing_wapp('core') as ctx:
+		assert ctx.wapp.route.mock_calls == [
+			call('/', 'GET', setup.redirect, name = 'setup.redir'),
+			call('/_/setup', 'GET', setup.index, name = 'setup'),
+			call('/_/setup/db/create', ['GET', 'POST'], setup.dbCreate, name = 'db.create'),
+			call('/<rpath:path>', 'GET', setup.redirect, name = 'setup.redirall'),
+		]
