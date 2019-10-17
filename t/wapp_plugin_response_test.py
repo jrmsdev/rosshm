f25bc0d076c6048df8e52cd356627cf89f5cb49d
Jeremías <jrmsdev@gmail.com>
2019-10-17 00:20:04 -0300

    check response headers are set

--- t/wapp_plugin_response_test.py
+++ t/wapp_plugin_response_test.py
@@ -26,6 +26,15 @@ def test_apply_http_response(testing_wapp_plugin):
 		assert isinstance(resp, bottle.HTTPResponse)
 		assert resp.status_code == 200
 		assert resp.body == 'testing'
+		_check_headers(resp.headers)
+
+def test_apply_debug(testing_wapp_plugin):
+	with testing_wapp_plugin() as ctx:
+		p = _new(ctx, debug = True)
+		ctx.callback.side_effect = _callback
+		resp = p.apply(ctx.callback, ctx.route)()
+		assert isinstance(resp, bottle.HTTPResponse)
+		_check_headers(resp.headers, debug = True)
 
 def test_apply(testing_wapp_plugin):
 	with testing_wapp_plugin() as ctx:
@@ -34,9 +43,10 @@ def test_apply(testing_wapp_plugin):
 		assert isinstance(resp, dict)
 		assert resp == {'tdata': 'testing'}
 
-def test_apply_debug(testing_wapp_plugin):
-	with testing_wapp_plugin() as ctx:
-		p = _new(ctx, debug = True)
-		resp = p.apply(ctx.callback, ctx.route)('testing')
-		assert isinstance(resp, dict)
-		assert resp == {'tdata': 'testing'}
+def _check_headers(h, debug = False):
+	assert h.get('server', 'NOTSET') == 'rosshm'
+	assert h.get('content-security-policy', False)
+	assert h.get('cache-control', False)
+	assert h.get('expires', False)
+	if debug:
+		assert h.get('x-took')
