2eed7efd23c8bb3ec4cc192aa7de1451559fa2fa
Jeremías <jrmsdev@gmail.com>
2019-10-16 23:35:21 -0300

    test db plugin apply

--- t/wapp_plugin_db_test.py
+++ t/wapp_plugin_db_test.py
@@ -2,10 +2,28 @@
 # See LICENSE file.
 
 from rosshm.wapp.plugin.db import DBPlugin
+from rosshm.db.conn import DBConn
+
+def _new(ctx):
+	debug = ctx.wapp.config.getbool('debug')
+	dbcfg = ctx.wapp.config.database()
+	return DBPlugin(dbcfg, debug)
 
 def test_setup(testing_wapp_plugin):
 	with testing_wapp_plugin() as ctx:
-		debug = ctx.wapp.config.getbool('debug')
-		dbcfg = ctx.wapp.config.database()
-		p = DBPlugin(dbcfg, debug)
+		p = _new(ctx)
 		p.setup(ctx.wapp.wapp)
+
+def _callback(db, *args, **kwargs):
+	tdata = kwargs.get('tdata', None)
+	return {'db': db, 'tdata': tdata}
+
+def test_apply(testing_wapp_plugin):
+	with testing_wapp_plugin() as ctx:
+		p = _new(ctx)
+		del ctx.callback
+		ctx.callback = _callback
+		d = p.apply(ctx.callback, ctx.route)(tdata = 'testing')
+		assert isinstance(d, dict)
+		assert d['tdata'] == 'testing'
+		assert isinstance(d['db'], DBConn)
--- tlib/testing/wapp_plugin_ctx.py
+++ tlib/testing/wapp_plugin_ctx.py
@@ -2,14 +2,21 @@
 # See LICENSE file.
 
 from contextlib import contextmanager
+from unittest.mock import Mock
 
 from testing.wapp_ctx import wapp_ctx
 
 class WappPluginCtx(object):
 	wapp = None
+	callback = None
+	route = None
 
 	def __init__(self, wapp):
 		self.wapp = wapp
+		self.callback = Mock()
+		self.callback.side_effect = lambda x: {'tdata': x}
+		self.route = Mock()
+		self.route.name = 'testing'
 
 @contextmanager
 def wapp_plugin_ctx():
